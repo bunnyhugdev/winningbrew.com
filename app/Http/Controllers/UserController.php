2506d231e544178abb3845545c087d38b521e50d
Joel Rathgaber <joel@lookagency.com>
2016-03-03 21:22:33 -0600

    fixed issue with selecting None as club

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,8 +35,8 @@ class UserController extends Controller {
         $user->province = $request->province;
         $user->postal_code = $request->postal_code;
         $user->accept_communication = $request->accept_communication;
-        $user->club_id = $request->club;
-        
+        $user->club_id = $request->club == "" ? null : $request->club;
+
         $user->save();
 
         $request->session()->flash('success', 'Your information has been updated');
