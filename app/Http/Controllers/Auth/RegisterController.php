79a1879a7638253bddf8428b0fc177c6da436f89
Joel Rathgaber <joel.rathgaber@hillbergandberk.com>
2018-04-15 09:58:26 -0600

    Upgrade to Laravel 5.6

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => Hash::make($data['password']),
         ]);
     }
 }
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -58,6 +58,7 @@ return [
         'array'   => 'The :attribute must have at least :min items.',
     ],
     'not_in'               => 'The selected :attribute is invalid.',
+    'not_regex'            => 'The :attribute format is invalid.',
     'numeric'              => 'The :attribute must be a number.',
     'regex'                => 'The :attribute format is invalid.',
     'required'             => 'The :attribute field is required.',
